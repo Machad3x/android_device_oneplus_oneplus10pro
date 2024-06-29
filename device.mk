d7b528b921daab4f13a89e5c75affbb33573628c
pjgowtham <pjgowtham@gmail.com>
2024-06-29 13:27:32 +0530

    oneplus10pro: Build only 64bit soundtrigger
    Change-Id: I87628335e28a55ec59d0efd4155b6f03c2551e6c

--- device.mk
+++ device.mk
@@ -56,7 +56,8 @@ PRODUCT_PACKAGES += \
     libagm_compress_plugin \
     libagm_mixer_plugin \
     libagm_pcm_plugin \
-    libpalclient
+    libpalclient \
+    sound_trigger.primary.taro:64
 
 # Authsecret
 PRODUCT_PACKAGES += \
